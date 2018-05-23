82041c154238d6d2498ea8fdaa774d75342a3fd7
Neil Patel <nmp61@pitt.edu>
2018-05-23 11:16:06 -0400

    Add Java Version

---
+++ FizzBuzz.java
@@ -0,0 +1,29 @@
+// Neil Patel
+// Java Implementation of FizzBuzz
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++){
+			fizzBuzz(i);
+		}
+	}
+
+	public static void fizzBuzz(int number) {
+		// Conditional to check if the number is divisble by both 3 and 5 first
+		if(number % 15 == 0) {
+			System.out.println("FizzBuzz");
+		
+		// Conditional to check if the number is divisble by 3
+		} else if (number % 3 == 0) {
+			System.out.println("Fizz");
+		
+		// Conditional to check if the number is divisble by 5
+		} else if (number % 5 == 0) {
+			System.out.println("Buzz");
+
+		// If none of the conditionals above are met, simply print the number
+		} else {
+			System.out.println(number);
+		}
+	}
+}
